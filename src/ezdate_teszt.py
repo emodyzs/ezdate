75e3b7d23225d44249cd359076f63d51b59f4143
emodyzs <emodyzs@gmail.com>
2022-01-18 13:38:54 +0100

    Add files via upload

--- src/ezdate_teszt.py
+++ src/ezdate_teszt.py
@@ -1,99 +1,4 @@
-import ezdate as d
-
-from datetime import datetime,date,timedelta
-
-d.text2date('Kérek egy kimutatást 2020 második féléve harmadik hetének közepéről. A nyári hónapokban lényegesen nagyobb volt a forgalom, ' +
-            'mint az előző években. Ha ugyanez a trend érvényesül, akkor idén nyáron még nagyobb forgalommal kell számolnunk.',outtype='all')
-
-
-
-d.text2date('2022 utáni 3 év',outtype='first+')     
-d.text2date('január 16 előtti első hétvége',outtype='first+')     
-d.text2date('jövő hónap első hétfőjén',outtype='first+')     
-
-
-d.text2date("két évvel ezelőtt 10-én",outtype='first+')     
-d.text2date("jövő hónap harmadikán",outtype='first+')     
-d.text2date("három évvel ezelőtt decemberben",outtype='first+')     
-d.text2date("március hónapban 5-én",outtype='first+')     
-
-d.text2date("2022 előtt",outtype='first+')     
-d.text2date("péntek előtt",outtype='first+')     
-d.text2date('múlt év előtt',outtype='first+')     
-d.text2date('jövő hónap után',outtype='first+')     
-d.text2date('holnap után',outtype='first+')     
-
-
-
-d.text2date("XIX. század 20-as éveiben",outtype='first+')     
-d.text2date("előző évszázad 20-as éveiben",outtype='first+')     
-d.text2date('1900-as évek',outtype='first+')     
-d.text2date('1990-es évek',outtype='first+')     
-d.text2date('2020-as évek',outtype='first+')     
-d.text2date("'20-as évek",outtype='first+')     
-d.text2date("huszas évek",outtype='first+')     
-d.text2date("90-es évek",outtype='first+')     
-d.text2date("30-as évek",outtype='first+')     
-
-d.text2date('két hónappal ezelőtt harmincadikán',outtype='first+')     
-d.text2date('három évvel ezelőtt decemberben',outtype='first+')
-d.text2date('három héttel ezelőtt szombaton',outtype='first+')
-
-# [szám] [időtartam] [múlvaezelőtt] [hónapnév]
-# [szám] [időtartam] [múlvaezelőtt] [hétnapja]
-
-d.text2date('három éve decemberben',outtype='first+')
-d.text2date('két hónapja hatodikán',outtype='first+')
-d.text2date('három hete pénteken',outtype='first+')
-
-d.text2date('három évvel ezelőtt december 5-én',outtype='first+')
-# [szám] [időtartam] [múlvaezelőtt] [hónapnév] [szám]
-
-d.text2date('két év múlva decemberben',outtype='first+')
-d.text2date('három hete pénteken',outtype='first+')
-
-d.text2date('két napja',outtype='first+')
-
-
-
-d.text2date('péntek előtt 3 héttel',outtype='first+')
-d.text2date('mától kezdve egy hét',outtype='first+')
-d.text2date('öt nappal március után',outtype='first+')
-d.text2date('jövő május második hetében',outtype='first+')
-d.text2date('jövő hétvége utáni második nap',outtype='first+')
-d.text2date('2023 második hónapjának harmadik hete',outtype='first+')
-
-h.fn_round(2.34)
-'01234'.isdigit()
-dt1=datetime.now()
-dt2=d.fn_dateadd(dt1,4)
-d.fn_daydiff(dt2,dt1)
-((dt2-dt1)/3) / timedelta(days=1)
-
-
-h.endwith('2.a',r'\.a')
-dt1=datetime(2022,2,1)
-dt1.weekday()
-d.fn_dateadd(dt1, (7 + (dt1.weekday()+1) - int(3)) % 7)
-
-15 % 4
-2010 % 10
-
-h.vanbenne('sokadik másodikán sok','kán|diká')
-samples='so|diká'
-m=re.search(r'(' + samples + ')','sokadik másodikán sok')
-m.group()
-
-(datetime(2022,3,31)-datetime(2022,1,1))/timedelta(days=1)
-
-8.4//7
-
-
-d_annotate('hétvége utáni nap',config.lookup_text2dateG,True)
-
-config.lookup_text2dateG
-
-h.kodto('3','1:egy//2:kettő')
+from ezdate as d
 
 
 tesztesetek=['ma',
@@ -119,8 +24,6 @@ for teszt in tesztesetek:
     print('"' + teszt + '"    ' + d.text2date(teszt))
 
 
-
-
-d_annotate('holnap után két nappal',config.lookup_text2dateG,True)
+input('\nBezárás: enter')
 
 
